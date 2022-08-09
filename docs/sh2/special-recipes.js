b1b69f9585b1aa1350fa3889520c6c9e1870ef92
aqiu384 <aqiu384@gmail.com>
2022-08-08 23:16:30 -0700

    sh2 ai-ho rename

--- docs/sh2/special-recipes.js
+++ docs/sh2/special-recipes.js
@@ -2,5 +2,5 @@ const SH2_SPECIAL_RECIPES = {
   "Yoshitsune": ["Yatagarasu", "Kurama Tengu", "Koppa Tengu"],
   "Take-Minakata": ["Koropokkur", "Hare of Inaba", "Onmoraki"],
   "Melchizedek": ["Angel", "Archangel"],
-  "Aiho": ["Jack Frost", "Cironnup"]
+  "Ai-ho": ["Jack Frost", "Cironnup"]
 }
