3f679e611fdd0b43a0792dbc393a35f4c4ae90d5
aqiu384 <aqiu384@gmail.com>
2024-03-11 13:48:18 -0700

    p3r skillcards

--- src/p3r/shared.py
+++ src/p3r/shared.py
@@ -23,3 +23,33 @@ def save_ordered_demons(demons, fname):
 
     with open(fname, 'w+') as jsonfile:
         jsonfile.write(jstring)
+
+ITEMS_DIR = 'Content/Xrd777/UI/Tables/DatItem{}NameDataAsset.tsv'
+ITEMS = [
+    ('Weapon',    0x0000),
+    ('Armor',     0x1000),
+    ('Shoes',     0x2000),
+    ('Accs',      0x3000),
+    ('Common',    0x4000),
+    ('Evitem',    0x5000),
+    ('Material',  0x6000),
+    ('Skillcard', 0x7000),
+    ('Costume',   0x8000)
+]
+
+def load_item_codes(language):
+    codes = {}
+    for fname, offset in ITEMS:
+        with open(ITEMS_DIR.format(fname)) as tsvfile:
+            col_index = next(tsvfile).strip().split('\t').index(language)
+            for i, line in enumerate(tsvfile):
+                iname = line.split('\t')[col_index].strip()
+                if iname != '未使用':
+                    codes[offset + i] = iname
+    return codes
+
+def iterate_int_tsvfile(fname):
+    with open(fname) as tsvfile:
+        headers = next(tsvfile).strip().split('\t')
+        for line in tsvfile:
+            yield { headers[i]: int(x) for i, x in enumerate(line.split('\t')) }
